545ca9bebed93a7b4ff8cfc1bba72daefe396f38
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-21 23:02:48 +0300

    implemented seller delete api

---
+++ ecommerce-server/admin/seller-delete.php
@@ -0,0 +1,45 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Content-Type:application/json');
+include '../database/Database.php';
+include '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$obj = new Database();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    try {
+        $allheaders = getallheaders();
+        $jwt =$allheaders['Authorization'];
+        $secret_key = "Hippo";
+        $user_data = JWT::decode($jwt, new Key($secret_key, 'HS256'));
+
+        if($user_data->data->user_type != 1) echo json_decode([
+            'status' => 0,
+            'message' => 'Access Denied',
+        ]);
+
+        $user_id = $_POST['id'];
+        $flag = 1;
+        $issues = [];
+            
+            $obj->delete('users', "id='{$user_id}'");
+            $result = $obj->getResult();
+            echo json_encode($result);
+
+    } catch (Exception $e) {
+        echo json_encode([
+            'status' => 0,
+            'message' => $e->getMessage(),
+        ]);
+    }
+} else {
+    echo json_encode([
+        'status' => 0,
+        'message' => 'Access Denied',
+    ]);
+}
