87e833a0897ff5d9af35791478968ce5afad96ba
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-21 11:43:43 +0300

    implemented barchart js

--- admin-electron/frontend/scripts/admin_infographics.js
+++ admin-electron/frontend/scripts/admin_infographics.js
@@ -4,3 +4,26 @@ google.charts.setOnLoadCallback(drawChart);
 const piechart = document.getElementById('piechart');
 const chart = document.getElementById('chart');
 const line_graph = document.getElementById('line_graph');
+
+
+function drawChart() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+            ['Shirts', 4],
+            ['Jeans', 2],
+            ['suits', .5],
+            ['Dresses', 1]
+        ]);
+
+        var piechart_options = {title:'Profit of each of the highest selling items',
+            width:540,
+            height:200,
+            is3D: true};
+        var piechart = new google.visualization.PieChart(document.getElementById('piechart'));
+        piechart.draw(data, piechart_options);
+
+
+}
