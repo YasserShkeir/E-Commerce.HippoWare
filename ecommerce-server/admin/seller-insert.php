dc911d53d37cfa941a538e79d2a20d6cec857a22
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-21 18:35:12 +0300

    added seller insert api

---
+++ ecommerce-server/admin/seller-insert.php
@@ -0,0 +1,58 @@
+<?php
+
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Content-Type:application/json');
+include '../database/Database.php';
+include '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$obj = new Database();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    try {
+        $allheaders = getallheaders();
+        $jwt =$allheaders['Authorization'];
+        $secret_key = "Hippo";
+        $user_data = JWT::decode($jwt, new Key($secret_key, 'HS256'));
+
+        if($user_data->data->user_type != 1) echo json_decode([
+            'status' => 0,
+            'message' => 'Access Denied',
+        ]);
+
+        $image = $_POST['image'];
+        $fname = $_POST['first_name'];
+        $lname = $_POST['last_name'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = hash("sha256",$_POST['password']);
+        $date = date('Y-m-d');
+
+        define('UPLOAD_DIR', '../images/');
+        $img = $image;
+        $img = str_replace('data:image/png;base64,', '', $img);
+        $img = str_replace(' ', '+', $img);
+        $data = base64_decode($img);
+        $file = UPLOAD_DIR . uniqid() . '.png';
+        $images_to_save = "/xampp/htdocs/E-Commerce.HippoWare/ecommerce-server/".$file;
+        $success = file_put_contents($file, $data);
+        
+        $obj->insert('users', ['user_type_id' => 2, 'first_name' => $fname, 'last_name' => $lname, 'username' => $username, 'email' => $email, 'password' => $password, 'image' => $image, 'accepted' => 1, 'date' => $date]);
+        $result = $obj->getResult();
+        echo json_encode($result);
+    } catch (Exception $e) {
+        echo json_encode([
+            'status' => 0,
+            'message' => $e->getMessage(),
+        ]);
+    }
+} else {
+    echo json_encode([
+        'status' => 0,
+        'message' => 'Access Denied',
+    ]);
+}
