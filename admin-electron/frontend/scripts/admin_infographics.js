1aeb1e56b08dcc904e76084048d42408933b808e
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-21 11:42:17 +0300

    implemented infographics js

---
+++ admin-electron/frontend/scripts/admin_infographics.js
@@ -0,0 +1,6 @@
+google.charts.load('current',{packages:['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+const piechart = document.getElementById('piechart');
+const chart = document.getElementById('chart');
+const line_graph = document.getElementById('line_graph');
