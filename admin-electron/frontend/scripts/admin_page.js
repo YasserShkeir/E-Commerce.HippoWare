530e64917619c3d91edc28d045273a8b68f1ab3b
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-21 08:43:36 +0300

    seperated html

--- admin-electron/frontend/scripts/admin_page.js
+++ admin-electron/frontend/scripts/admin_page.js
@@ -3,13 +3,13 @@ const clients_container =  document.getElementById('clients_container');
 const header_clients_btn =  document.getElementById('header_clients_btn');
 const header_sellerss_btn =  document.getElementById('header_sellerss_btn');
 
-header_sellerss_btn.addEventListener('click', ()=>{
-    sellers_container.style.display = 'flex';
-    clients_container.style.display = 'none';
-});
+// header_sellerss_btn.addEventListener('click', ()=>{
+//     sellers_container.style.display = 'flex';
+//     clients_container.style.display = 'none';
+// });
 
-header_clients_btn.addEventListener('click', ()=>{
-    clients_container.style.display = 'flex';
-    sellers_container.style.display = 'none';
-});
+// header_clients_btn.addEventListener('click', ()=>{
+//     clients_container.style.display = 'flex';
+//     sellers_container.style.display = 'none';
+// });
 
