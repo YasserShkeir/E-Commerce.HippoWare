2fb0b110a1fabddce0df5ae222b5c860b17c0410
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-21 15:02:50 +0300

    created navbar elements through js

---
+++ seller-frontend/scripts/home_page.js
@@ -0,0 +1,13 @@
+const nav_bar = document.getElementById('nav-bar');
+
+const logo_img = document.createElement('img');
+
+const logo_img = document.createElement('img');
+    const logo_img = document.createElement('img');
+    const logo_img = document.createElement('img');
+
+const logo_img = document.createElement('img');
+    const logo_img = document.createElement('img');
+    const logo_img = document.createElement('img');
+
+const logo_img = document.createElement('img');
