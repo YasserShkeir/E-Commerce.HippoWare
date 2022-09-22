80fd1b49db1db43957ba2d2b7192244f5929db9e
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-22 21:49:15 +0300

    added add product api

--- ecommerce-server/seller/add-product.php
+++ ecommerce-server/seller/add-product.php
@@ -20,51 +20,57 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $user_data = JWT::decode($jwt, new Key($secret_key, 'HS256'));
         $request_body = file_get_contents('php://input');
         $data = json_decode($request_body, true);
-
-        if($user_data->data->user_type != 1) echo json_decode([
+        print gettype($data);
+        if($user_data->data->user_type != 2) echo json_decode([
             'status' => 0,
             'message' => 'Access Denied',
         ]);
+        $category = $data["category"];
+        $price = $data["price"];
+        $name = $data['name'];
+        $description = $data["description"];
+        $color = $data['color'];
+        $size = $data["size"];
+        $revenue = $data["revenue"];
 
-        $image = $data['image'];
-        $fname = $data['first_name'];
-        $lname = $data['last_name'];
-        $username = $data['username'];
-        $email = $data['email'];
-        $password = hash("sha256",$data['password']);
-        $date = date('Y-m-d');
-        $flag = 1;
-        $issues = [];
-
-        $obj->select('users', '*', null, "email='{$email}'", null, null);
+        $obj->select('`stores`','id', null, "seller_id = ".$user_data->data->id, null, null);// getting store id of user
         $result = $obj->getResult();
-        if($result){
-            $flag=0;
-            $issues[] = "dup email";
-        }
-        $obj->select('users', '*', null, "username='{$username}'", null, null);
-        $result = $obj->getResult();
-        if($result){
-            $flag=0;
-            $issues[] = "dup username";
-        }
+        $storeid = $result[0]['id'];
 
+        $where = "store_id = " . $storeid . " and name = '$category'";
+
+        $obj->select('`categories`', 'id', null, $where, null, null);// checking if category already exists
+        $result = $obj->getResult();
+        $catid = $result[0]['id'];
+
+        // image decoding
         define('UPLOAD_DIR', '../images/');
-        $img = $image;
+        $img = $data['image'];
         $img = str_replace('data:image/png;base64,', '', $img);
         $img = str_replace(' ', '+', $img);
         $data = base64_decode($img);
         $file = UPLOAD_DIR . uniqid() . '.png';
         $images_to_save = "/xampp/htdocs/E-Commerce.HippoWare/ecommerce-server/".$file;
-        
-        if($flag){
-            $obj->insert('users', ['user_type_id' => 2, 'first_name' => $fname, 'last_name' => $lname, 'username' => $username, 'email' => $email, 'password' => $password, 'image' => $images_to_save, 'accepted' => 1, 'date' => $date]);
-            $result = $obj->getResult();
-            file_put_contents($file, $data);
-            echo json_encode($result);
-        }else {
-            echo json_encode($issues);
-        }
+
+        print $price;
+
+        $insertArr = [
+            'category_id' => $catid,
+            'store_id' => $storeid,
+            'name' => $name,
+            'price' =>$price,
+            'image' =>	$images_to_save,
+            'description' =>$description,
+            'color' => $color,
+            'size' => $size,
+            'views' => 0,
+            'revenue' =>$revenue
+        ];
+
+        $obj->insert('products',$insertArr );
+        $result = $obj->getResult();
+        file_put_contents($file, $data);
+        echo json_encode($result);
     } catch (Exception $e) {
         echo json_encode([
             'status' => 0,
