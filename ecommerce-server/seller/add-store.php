7aba3d7714fcb3bdd1f08bda3222e5b20efd5807
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-23 00:44:40 +0300

    added store add api and store list api

--- ecommerce-server/seller/add-store.php
+++ ecommerce-server/seller/add-store.php
@@ -21,31 +21,27 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $request_body = file_get_contents('php://input');
         $data = json_decode($request_body, true);
 
-        if($user_data->data->user_type != 3)
-        echo json_encode([
+        if($user_data->data->user_type != 2) echo json_encode([
             'status' => 0,
             'message' => 'Access Denied',
         ]);
+        $name = $data["name"];
+        $welc_msg = $data["welc_msg"];
 
-        $reciever = $data['username'];
-        $message = $data['message'];
-        $value = $data['value'];
+        define('UPLOAD_DIR', '../images/');
+        $img = $data['image'];
+        $img = str_replace('data:image/png;base64,', '', $img);
+        $img = str_replace(' ', '+', $img);
+        $data = base64_decode($img);
+        $file = UPLOAD_DIR . uniqid() . '.png';
+        $images_to_save = "/xampp/htdocs/E-Commerce.HippoWare/ecommerce-server/".$file;
 
-        $obj->select('`users`','id', null, "username = '".$reciever."'", null, null);// getting store id of user
+
+        $obj->insert('stores',['seller_id' => $user_data->data->id,'name' => $name , 'image' => $images_to_save, 'welc_msg' => $welc_msg]);
         $result = $obj->getResult();
+        file_put_contents($file, $data);
+        echo json_encode($result);
 
-        if(!$result){ // if category exits insert fails
-            echo json_encode([
-                'status' => 0,
-                'message' => 'user does not exist',
-            ]);
-        }
-        else{
-            $recieverid = $result[0]['id'];
-            $obj->insert('vouchers',['sender_id' => $user_data->data->id, 'reciever_id' => $recieverid, 'message' => $message, 'value' => $value]);
-            $result = $obj->getResult();
-            echo json_encode($result);
-        }
     } catch (Exception $e) {
         echo json_encode([
             'status' => 0,
