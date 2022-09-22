9c85ccaebcd659b95da458850f35bb4ee6a18c27
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-23 02:16:05 +0300

    Navbar Edit - Responsive Mobile Burger Menu Edit

--- client-frontend/scripts/searchResults.js
+++ client-frontend/scripts/searchResults.js
@@ -1,22 +1,69 @@
 window.onload = () => {
-  const navBar = document.querySelector(".client-nav");
-  navBar.innerHTML = `<a href="landingPage.html"><img src="../assets/images/logo-removebg-preview.png" /></a>
-        <div class="client-nav-links flex">
-          <div class="client-nav-link">
-            <a href="editProfile.html">Profile</a>
-          </div>
-          <div class="client-nav-link">
-            <a href="wishlist.html">Wishlist</a>
-          </div>
-          <div class="client-nav-link">
-            <a href="favorites.html">Favorites</a>
-          </div>
-        </div>
-        <div class="client-nav-search">
-          <i class="fa fa-search"></i>
-          <input id="search" type="text" placeholder="Search" />
-        </div>
-        <div id="logOut" class="client-nav-link">Log Out</div>
-        <div class="client-nav-link"><a href="cart.html"><i class="fa fa-shopping-cart"></i></a></div>
-      `;
+  const navBarCaller = () => {
+    const navBar = document.querySelector(".client-nav");
+    navBar.innerHTML = `<a href="landingPage.html"
+    ><img id="navLogo" src="../assets/images/logo-removebg-preview.png"
+  /></a>
+  <div class="client-nav-links flex">
+    <div class="client-nav-link">
+      <a href="editProfile.html">Profile</a>
+    </div>
+    <div class="client-nav-link">
+      <a href="wishlist.html">Wishlist</a>
+    </div>
+    <div class="client-nav-link">
+      <a href="favorites.html">Favorites</a>
+    </div>
+  </div>
+  <div class="client-nav-search">
+    <i class="fa fa-search"></i>
+    <input id="search" type="text" placeholder="Search" />
+  </div>
+  
+  <div class="mobile-links">
+    <div class="mobile-client-nav-link">
+      <a href="editProfile.html">Profile</a>
+    </div>
+    <div class="mobile-client-nav-link">
+      <a href="wishlist.html">Wishlist</a>
+    </div>
+    <div class="mobile-client-nav-link">
+      <a href="favorites.html">Favorites</a>
+    </div>
+    <div class="mobile-client-nav-link">
+      <a href="cart.html"><i class="fa fa-shopping-cart"></i></a>
+    </div>
+  </div>
+  
+  <div class="logOut" class="client-nav-link">Log Out</div>
+  <div class="client-nav-link">
+    <a href="cart.html"><i class="fa fa-shopping-cart"></i></a>
+  </div>
+  <div id="navMenu">
+    <button id="navMenuBTN">
+      <i class="fa fa-bars" style="font-size: 20px"></i>
+    </button>
+  </div>`;
+
+    const menuClicker = document.querySelector("#navMenuBTN");
+    const searchBar = document.querySelector(".client-nav-search");
+    const mobileNav = document.querySelector(".mobile-links");
+    const logout = document.querySelector(".logOut");
+
+    console.log(menuClicker);
+
+    menuClicker.addEventListener("click", () => {
+      if (mobileNav.style.display == "flex") {
+        mobileNav.style.display = "none";
+        searchBar.style.display = "flex";
+        logout.style.display = "flex";
+      } else {
+        mobileNav.style.display = "flex";
+        searchBar.style.display = "none";
+        logout.style.display = "none";
+      }
+    });
+  };
+
+  navBarCaller();
 };
