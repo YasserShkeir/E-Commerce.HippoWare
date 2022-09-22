bc597204c88264eeed5e100507b9cce87a445765
noormesslmani <noor.messlmani@gmail.com>
2022-09-22 07:23:26 +0300

    displaying products through js intead of hard coding

--- seller-frontend/scripts/products.js
+++ seller-frontend/scripts/products.js
@@ -1,5 +1,21 @@
 window.onload = () => {
-    
+    const categorySelect= document.getElementById("category-select")
+    const addCategory= document.getElementById("add-category")
+    const addProduct= document.getElementById("add-product")
+    const products= document.getElementById("products")
 
-
+    const productCard = 
+    `<div class="product-card">
+        <img src="../../assets/jacket2.jfif" class="jacket"> 
+        <div class="product-details">
+            <div>Product Name</div>
+            <div>Colors: black, white, red, blue</div>
+            <div>Sizes:S,M,L,XL,XXL</div>
+            <div>Revenue:</div>
+            <div>Price:</div>
+        </div>
+    </div>`
+    for(let i=0;i<6;i++){
+        products.innerHTML+=productCard
+    }
 }
