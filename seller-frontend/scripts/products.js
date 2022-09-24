6fb2b55eb23b1c9ebca3640c07a32a2701ee7476
noormesslmani <noor.messlmani@gmail.com>
2022-09-24 09:18:07 +0300

    implemented upload new product feature for seller

--- seller-frontend/scripts/products.js
+++ seller-frontend/scripts/products.js
@@ -8,10 +8,17 @@ window.onload = () => {
     const deleteModal= document.getElementById("deletion-modal")
     const deleteItem= document.getElementById("delete-item")
     const cancelDelete= document.getElementById("cancel-delete")
-    const trashBin=document.querySelectorAll(".trash-bin")
-    
-    console.log(typeof(trashBin))
-
+    const trashBin = document.getElementsByClassName("trash-bin")
+    const logOut=document.getElementById('log-out')
+    const logoutModal=document.getElementById('logout-modal')
+    const cancelLogout=document.getElementById("cancel-logout")
+    const confirmLogout=document.getElementById("confirm-logout")
+    const dropDownContents=document.getElementById('drop')
+    const categoryOptions=document.getElementsByClassName('category-options')
+    const customizeCategory=document.getElementById('customize-category')
+    const uploadProduct=document.getElementById('uploadproduct-modal')
+    const submitUpload= document.getElementById('submit-upload')
+    const cancelUpload= document.getElementById('cancel-upload')
     //display all products
     if (categorySelect.value== 'none'){
         displaybyCategroy('0')
@@ -23,66 +30,92 @@ window.onload = () => {
         displaybyCategroy(categorySelected)
     }
 
-    function displaybyCategroy(categorySelected){
+    async function displaybyCategroy(categorySelected){
         let payload = {search:"0",
         category:categorySelected}
         let config = {
             headers: {'Authorization': localStorage.jwt}
         }
-        let res = axios.post('http://localhost/E-Commerce.HippoWare/ecommerce-server/seller/products.php',payload,config).then(
+        let res =  await axios.post('http://localhost/E-Commerce.HippoWare/ecommerce-server/seller/products.php',payload,config).then(
             function (response) {
-                for (data of response.data){
-                    displayProducts(data) 
-                }
-                console.log(trashBin.length)
+               
+                displayProducts(response.data)
                 return response.data
             })
             .catch(function (error) {
                 console.log(error);
             })
         }
-    function displayProducts(data){
-        let productCard = 
-            `<div class="product-card">
-                <img src="../../assets/jacket2.jfif" class="jacket"> 
-                <div class="product-details">
-                    <div>Name : ${data.name}</div>
-                    <div>Color: ${data.color} </div>
-                    <div>Size: ${data.size}</div>
-                    <div>Revenue: ${data.revenue}</div>
-                    <div>Price: ${data.price}</div>
-                    <img src="../../assets/trash.png" class="trash-bin">
-                </div>
-            </div>`
-            products.innerHTML+=productCard
+    function displayProducts(response){
+        for (data of response){
+            let productCard = 
+                `<div class="product-card">
+                    <img src="../../assets/jacket2.jfif" class="jacket"> 
+                    <div class="product-details">
+                        <div>Name : ${data.name}</div>
+                        <div>Color: ${data.color} </div>
+                        <div>Size: ${data.size}</div>
+                        <div>Revenue: ${data.revenue}</div>
+                        <div>Price: ${data.price}</div>
+                        <img src="../../assets/trash.png" class="trash-bin">
+                    </div>
+                </div>`
+                products.innerHTML+=productCard
+        }
     }
-    
-    
-    // let payload = {search:"0", category:'0'}
-    // let config = {
-    //     headers: {'Authorization': localStorage.jwt}
-    // }
-    // let res = axios.post('http://localhost/E-Commerce.HippoWare/ecommerce-server/seller/products.php',payload,config).then(
-    //     function (response) {
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     })
-    
-   
-    // function deleteProduct(){
-    //     displayedProducts.onclick=(e)=>{
-    //         if (e.target)
-    //     }
-    // }
 
 
-    
+    //adding a new product
+    const uploadCategory=document.getElementById('upload-category')
+    const uploadPrice=document.getElementById('upload-price')
+    const uploadDiscount=document.getElementById('upload-discount')
+    const uploadName=document.getElementById('upload-name')
+    const uploadColor=document.getElementById('upload-color')
+    const uploadSize=document.getElementById('upload-size')
+    const uploadDescription=document.getElementById('upload-description')
+    const uploadRevenue=document.getElementById('upload-revenue')
 
+    addProduct.onclick=()=>{
+        uploadProduct.style.display="Block"
+    }
+    cancelUpload.onclick=()=>{
+        uploadProduct.style.display="none"
+    }
+    submitUpload.onclick=(e)=>{
+        e.preventDefault()
+        console.log(uploadCategory.value)
+        console.log(uploadColor.value)
+        if (uploadCategory.value && uploadPrice.value && uploadColor.value && uploadName.value && 
+            uploadSize.value && uploadDescription.value &&uploadDiscount.value){
+                console.log(uploadCategory.value)
+                console.log(uploadColor.value)
+                uplaodNewPorduct()
+                uploadProduct.style.display="none"
+        }
+    }
+    function uplaodNewPorduct(){
+        let payload = {image:"null", 
+        category:uploadCategory.value,
+        price:uploadPrice.value,
+        revenue:uploadRevenue.value,
+        name: uploadName.value,
+        description: uploadDescription.value,
+        color:uploadColor.value,
+        size: uploadSize.value
+    }
+        let config = {
+            headers: {'Authorization': localStorage.jwt}
+        }
+        let res =  axios.post('http://localhost/E-Commerce.HippoWare/ecommerce-server/seller/add-product.php',payload,config).then(
+            function (response) {
+                return response.data
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+        
 
-
-    
-    
 
     // for (let displayedProduct of displayedProducts){
     //     displayedProduct.onclick=(e)=>{
@@ -140,10 +173,6 @@ window.onload = () => {
     }
 
     //adding categories
-
-    const dropDownContents=document.getElementById('drop')
-    const categoryOptions=document.getElementsByClassName('category-options')
-    const customizeCategory=document.getElementById('customize-category')
     addCategory.onmouseover=()=>{
         dropDownContents.style.display="Block"
     }
@@ -212,13 +241,6 @@ window.onload = () => {
     //     }
     // }
 
-    addProduct.onclick=()=>{  }
-
-
-    const logOut=document.getElementById('log-out')
-    const logoutModal=document.getElementById('logout-modal')
-    const cancelLogout=document.getElementById("cancel-logout")
-    const confirmLogout=document.getElementById("confirm-logout")
     logOut.onclick=()=>{
         logoutModal.style.display='Block'
         cancelLogout.onclick=()=>{logoutModal.style.display='none'}
