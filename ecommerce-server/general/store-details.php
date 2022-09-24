a7cbad05b150356c817cf303b2b0a484acb076e5
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-24 16:01:15 +0300

    added store details and products for clients

---
+++ ecommerce-server/general/store-details.php
@@ -0,0 +1,32 @@
+<?php
+
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Content-Type:application/json');
+include '../database/Database.php';
+include '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$obj = new Database();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body, true);
+
+    $storeid=$data["storeid"];
+    $where = "id = ". $storeid;
+        
+    $obj->select('`stores`','*', null, $where, null, null);
+    $result = $obj->getResult();
+    echo json_encode($result);
+
+} else {
+    echo json_encode([
+        'status' => 0,
+        'message' => 'Access Denied',
+    ]);
+}
+?>
