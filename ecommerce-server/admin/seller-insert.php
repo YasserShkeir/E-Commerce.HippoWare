9b00d851901b5e6e2bc348d12e15d25fa96a8a79
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-24 17:06:12 +0300

    tweaked image path

--- ecommerce-server/admin/seller-insert.php
+++ ecommerce-server/admin/seller-insert.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $img = str_replace('data:image/png;base64,', '', $img);
         $img = str_replace(' ', '+', $img);
         $data = base64_decode($img);
-        $file = UPLOAD_DIR . uniqid() . '.png';
+        $file = "../".UPLOAD_DIR . uniqid() . '.png';
         $images_to_save = "/xampp/htdocs/E-Commerce.HippoWare/ecommerce-server/".$file;
         
         if($flag){
