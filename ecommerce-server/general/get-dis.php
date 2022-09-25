08346a3f1ed7261c98680637c6ae11fd50456f8e
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-25 23:47:44 +0300

    finished store page except chat(very broken)

---
+++ ecommerce-server/general/get-dis.php
@@ -0,0 +1,37 @@
+<?php
+
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Content-Type:application/json');
+include '../database/Database.php';
+include '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$obj = new Database();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    try {
+        $request_body = file_get_contents('php://input');
+        $data = json_decode($request_body, true);
+        $store = $data["store"];
+
+        $obj->select('`discountcodes`','*', null, "store_id = ".$store, "discount Desc", "0,1");// getting messages
+        $result = $obj->getResult();
+        echo json_encode($result);
+
+    } catch (Exception $e) {
+        echo json_encode([
+            'status' => 0,
+            'message' => $e->getMessage(),
+        ]);
+    }
+} else {
+    echo json_encode([
+        'status' => 0,
+        'message' => 'Access Denied',
+    ]);
+}
+?>
