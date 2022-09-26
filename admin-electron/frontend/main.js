e2340724f20b2008c2fadfde512ed6665840bffa
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-27 00:06:29 +0300

    Adding Node Modules for Electron

--- admin-electron/frontend/main.js
+++ admin-electron/frontend/main.js
@@ -1,31 +1,32 @@
-const {BrowserWindow, app, Menu} = require('electron');
-let login_page = 'index.html';
+const { BrowserWindow, app, Menu } = require("electron");
+let login_page = "./html/index.html";
 
+const createWindow = (path) => {
+  win = new BrowserWindow({
+    show: false,
+    icon: "assets/images/hippo-icon.png",
+  });
+  win.maximize();
+  win.show();
+  win.loadFile(path);
+  // win.webContents.openDevTools();
+  const builtInMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(builtInMenu);
+};
 
-const createWindow = (path) =>{
-    win = new BrowserWindow({show: false,
-        icon:'assets/images/hippo-icon.png'});
-    win.maximize();
-    win.show();
-    win.loadFile(path);
-    // win.webContents.openDevTools();
-    const builtInMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(builtInMenu);
-}
-
-app.whenReady().then( ()=> {
-    createWindow(login_page);
+app.whenReady().then(() => {
+  createWindow(login_page);
 });
 
 // mac os exception
 // because software will keep running on mac even when all windows are closed
-app.on('window-all-closed', () =>{
-    if(process.platform !== 'darwin') app.quit();
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // REMOVE BUILTIN MENU
 const mainMenuTemplate = [
-    {
-        label: ''
-    }
+  {
+    label: "",
+  },
 ];
