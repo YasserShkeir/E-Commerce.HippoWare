dc3c38b0b0dfd528941522d2573138a379e89924
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-26 13:23:03 +0300

    changed store link

--- client-frontend/scripts/landingPage.js
+++ client-frontend/scripts/landingPage.js
@@ -120,7 +120,7 @@ window.onload = () => {
     if (flag) {
       main.addEventListener("click", () => {
         localStorage.setItem("product", data["id"]);
-        window.open("client-frontend/html/sellerProfile.html", "_self");
+        window.open("sellerProfile.html", "_self");
       });
       div.appendChild(main);
       return;
